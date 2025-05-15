bd66284550d9a2d4f7eb3fdd2ba4259751464f31
Arsema Addis <arsemadd@gmail.com>
2025-05-15 13:09:20 +0300

    create service

---
+++ src/main/java/com/example/library/service/AuthorService.java
@@ -0,0 +1,36 @@
+package com.example.library.service;
+
+import com.example.library.entity.Author;
+import com.example.library.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public Optional<Author> getAuthorById(Long id) {
+        return authorRepository.findById(id);
+    }
+
+    public Author saveAuthor(Author author) {
+        return authorRepository.save(author);
+    }
+
+    public void deleteAuthor(Long id) {
+        authorRepository.deleteById(id);
+    }
+}
